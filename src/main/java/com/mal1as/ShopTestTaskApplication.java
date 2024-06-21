7eff780e2d61013ea31ffca67f0649df79e44716
mal1as <mishadyakonov14050@gmail.com>
2024-06-21 18:05:02 +0300

    Add dependencies for Spring

---
+++ src/main/java/com/mal1as/ShopTestTaskApplication.java
@@ -0,0 +1,12 @@
+package com.mal1as;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopTestTaskApplication.class, args);
+    }
+}
